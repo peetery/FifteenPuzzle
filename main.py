124f4065d525dc6597d338c7a597c1514ba2531f
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-04-09 14:37:21 +0200

    Class Puzzle: expension of move method implementation. Added moves to class Puzzle to save all moves directions

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     puzzle = Puzzle((0, 0), [])
     puzzle.load_from_file("startPuzzle.txt")
     print(puzzle)
-    print("Rozmiar tablicy: ", puzzle.size)
+    print("Rozmiar tablicy: ", puzzle.size[0])
--- puzzle.py
+++ puzzle.py
@@ -1,8 +1,11 @@
+import numpy as np
+
 class Puzzle:
     def __init__(self, size, initial_state):
         self.size = size
         self.initial_state = initial_state
         self.current_state = initial_state
+        self.moves = ""
 
     def load_from_file(self, filename):
         with open(filename, 'r') as file:
@@ -17,7 +20,7 @@ class Puzzle:
         return output
 
     def move(self, direction):
-        empty_row, empty_col = None, None
+        empty_row, empty_col, new_row, new_col = None, None, None, None
         for i in range(self.size[0]):
             for j in range(self.size[1]):
                 if self.current_state[i][j] == 0:
@@ -26,13 +29,22 @@ class Puzzle:
             if empty_row is not None:
                 break
 
-        # ! niedokończone, patrz niżej
-        if direction == "L":
+        new_state = np.copy(self.current_state)
+
+        if direction == "L" and empty_col > 0:
             new_row, new_col = empty_row, empty_col - 1
-        elif direction == "R":
+        elif direction == "R" and empty_col < self.size[1] - 1:
             new_row, new_col = empty_row, empty_col + 1
-        elif direction == "U":
+        elif direction == "U" and empty_row < self.size[0] - 1:
             new_row, new_col = empty_row + 1, empty_col
-        elif direction == "D":
+        elif direction == "D" and empty_row > 0:
             new_row, new_col = empty_row - 1, empty_col
-        # teraz trzeba jeszcze pomyśleć nad zabezpieczeń aby nie wyjść poza granice planszy
+
+        new_state[empty_row][empty_col] = new_state[new_row][new_col]
+        new_state[new_row][new_col] = new_state[empty_row][empty_col]
+
+        # sprawdzamy czy nowy stan różni się od obecnego, jest to przydatne aby uniknąć bezcelowego kopiowania
+        # gdy zadany ruch wykracza poza granice planszy i nie zostanie wykonany
+        if not np.array_equal(self.current_state, new_state):
+            self.current_state = new_state
+            self.moves += direction
