de4c9810df4ef1a1df27bbfb2e1a03ceab222900
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-04-09 13:27:28 +0200

    Basic implementation of Puzzle class. Added loading puzzle from file.

--- puzzle.py
+++ puzzle.py
@@ -1,2 +1,17 @@
+class Puzzle:
+    def __init__(self, size, initial_state):
+        self.size = size
+        self.initial_state = initial_state
+        self.current_state = initial_state
 
-            
+    def load_from_file(self, filename):
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            self.size = tuple(map(int, lines[0].split()))
+            self.current_state = [list(map(int, line.split())) for line in lines[1:]]
+
+    def __str__(self):
+        output = ""
+        for row in self.current_state:
+            output += " ".join(str(num) for num in row) + "\n"
+        return output
