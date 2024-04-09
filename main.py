59acc455d2dd9c077bd5e3d20a3d28aa306912e9
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-04-09 15:09:33 +0200

    Class Puzzle: fixed move method, added methods get_target_stated, is_solved and get_neighbors

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from puzzle import Puzzle
 if __name__ == '__main__':
     puzzle = Puzzle((0, 0), [])
     puzzle.load_from_file("startPuzzle.txt")
+    print("Aktualny stan:")
     print(puzzle)
-    print("Rozmiar tablicy: ", puzzle.size[0])
+    print("Docelowy stan:")
+    print(puzzle.get_target_state())
+    print("\nis solved: ", puzzle.is_solved())
+
+    for neighbor, direction in puzzle.get_neighbors():
+        print(neighbor, "Ruch:", direction)
--- puzzle.py
+++ puzzle.py
@@ -19,8 +19,8 @@ class Puzzle:
             output += " ".join(str(num) for num in row) + "\n"
         return output
 
-    def move(self, direction):
-        empty_row, empty_col, new_row, new_col = None, None, None, None
+    def find_empty_cell(self):
+        empty_row, empty_col = None, None
         for i in range(self.size[0]):
             for j in range(self.size[1]):
                 if self.current_state[i][j] == 0:
@@ -28,23 +28,64 @@ class Puzzle:
                     break
             if empty_row is not None:
                 break
+        return empty_row, empty_col
+
+    def move(self, direction):
+        empty_row, empty_col = self.find_empty_cell()
 
         new_state = np.copy(self.current_state)
+        new_row, new_col = empty_row, empty_col
 
         if direction == "L" and empty_col > 0:
-            new_row, new_col = empty_row, empty_col - 1
+            new_col -= 1
         elif direction == "R" and empty_col < self.size[1] - 1:
-            new_row, new_col = empty_row, empty_col + 1
+            new_col += 1
         elif direction == "U" and empty_row < self.size[0] - 1:
-            new_row, new_col = empty_row + 1, empty_col
+            new_row -= 1
         elif direction == "D" and empty_row > 0:
-            new_row, new_col = empty_row - 1, empty_col
+            new_row += 1
 
         new_state[empty_row][empty_col] = new_state[new_row][new_col]
-        new_state[new_row][new_col] = new_state[empty_row][empty_col]
+        new_state[new_row][new_col] = self.current_state[empty_row][empty_col]
 
         # sprawdzamy czy nowy stan różni się od obecnego, jest to przydatne aby uniknąć bezcelowego kopiowania
         # gdy zadany ruch wykracza poza granice planszy i nie zostanie wykonany
         if not np.array_equal(self.current_state, new_state):
             self.current_state = new_state
             self.moves += direction
+
+    def get_target_state(self):
+        target_state = []
+        for i in range(self.size[0]):
+            row = []
+            for j in range(self.size[1]):
+                row.append(i * self.size[1] + j + 1)
+            target_state.append(row)
+        target_state[-1][-1] = 0
+        return target_state
+
+    def is_solved(self):
+        return np.array_equal(self.current_state, self.get_target_state())
+
+    def get_neighbors(self):
+        neighbors = []
+        empty_row, empty_col = self.find_empty_cell()
+        for direction in ["L", "R", "U", "D"]:
+            new_row, new_col = empty_row, empty_col
+            if direction == "L" and empty_col > 0:
+                new_col -= 1
+            elif direction == "R" and empty_col < self.size[1] - 1:
+                new_col += 1
+            elif direction == "U" and empty_row > 0:
+                new_row -= 1
+            elif direction == "D" and empty_row < self.size[0] - 1:
+                new_row += 1
+
+            if new_row != empty_row or new_col != empty_col:
+                neighbor = np.copy(self.current_state)
+                neighbor[empty_row][empty_col] = neighbor[new_row][new_col]
+                neighbor[new_row][new_col] = self.current_state[empty_row][empty_col]
+                neighbors.append((neighbor, direction))
+
+        return neighbors
+
