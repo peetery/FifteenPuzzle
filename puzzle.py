fd2c1fc6b729b6b903bf5851cb0bf5d573342f85
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-04-09 13:40:53 +0200

    Class Puzzle: added move method

--- puzzle.py
+++ puzzle.py
@@ -15,3 +15,24 @@ class Puzzle:
         for row in self.current_state:
             output += " ".join(str(num) for num in row) + "\n"
         return output
+
+    def move(self, direction):
+        empty_row, empty_col = None, None
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                if self.current_state[i][j] == 0:
+                    empty_row, empty_col = i, j
+                    break
+            if empty_row is not None:
+                break
+
+        # ! niedokończone, patrz niżej
+        if direction == "L":
+            new_row, new_col = empty_row, empty_col - 1
+        elif direction == "R":
+            new_row, new_col = empty_row, empty_col + 1
+        elif direction == "U":
+            new_row, new_col = empty_row + 1, empty_col
+        elif direction == "D":
+            new_row, new_col = empty_row - 1, empty_col
+        # teraz trzeba jeszcze pomyśleć nad zabezpieczeń aby nie wyjść poza granice planszy
