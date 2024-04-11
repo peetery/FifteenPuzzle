17e6fd551b7f492d7dd5d04b861584a4d779ee8a
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-04-11 11:31:04 +0200

    Utils class: added methods to calculate manhattan distance and hamming distance.

--- utils.py
+++ utils.py
@@ -13,3 +13,25 @@ class Utils:
             file.write(f"{size[0]} {size[1]}\n")
             for row in state:
                 file.write(" ".join(str(num) for num in row) + "\n")
+
+    @staticmethod
+    def hamming_distance(state, target_state):
+        distance = 0
+        for i in range(len(state)):
+            for j in range(len(state[i])):
+                if state[i][j] != target_state[i][j]:
+                    distance += 1
+        return distance
+
+    # ta implementacja manhattan distance jeszcze do przemyślenia / ewentualnej zmiany
+    @staticmethod
+    def manhattan_distance(state, target_state):
+        distance = 0
+        size = len(state)
+        for i in range(size):
+            for j in range(size):
+                num = state[i][j]
+                if num != 0:
+                    target_row, target_col = divmod(num - 1, size)
+                    distance += abs(i - target_row) + abs(j - target_col)
+        return distance
